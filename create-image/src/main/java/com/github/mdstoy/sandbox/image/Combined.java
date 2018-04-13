070ab921e9f4c53936366fd8a5aeb570e5648ba2
mdstoy <toyofyou.me@gmail.com>
2018-04-13 14:54:29 +0900

    画像がぐちゃぐちゃになる

---
+++ create-image/src/main/java/com/github/mdstoy/sandbox/image/Combined.java
@@ -0,0 +1,48 @@
+package com.github.mdstoy.sandbox.image;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class Combined {
+
+    public static void main(String[] args) throws Exception {
+        new Combined().execute();
+    }
+
+    public void execute() throws Exception{
+
+        BufferedImage bufferedImage1 = ImageIO.read(new File("back.png"));
+        BufferedImage bufferedImage2 = ImageIO.read(new File("down.png"));
+
+        Graphics2D graphics1 = null;
+        Graphics2D graphics2 = null;
+
+        try {
+
+            graphics1 = bufferedImage1.createGraphics();
+
+            int x = 0;
+            int y = 0;
+            graphics1.drawImage(bufferedImage2, x, y, null);
+
+            BufferedImage bufferedImage3 = ImageIO.read(new File("down.png"));
+            graphics2 = bufferedImage3.createGraphics();
+            AffineTransform at = new AffineTransform();
+            at.setToRotation(90 * Math.PI/180, 24d, 24d);
+            graphics2.setTransform(at);
+            graphics2.drawImage(bufferedImage3, null, 0, 0);
+            graphics1.drawImage(bufferedImage3, 48, 0, null);
+
+        } finally {
+
+            graphics1.dispose();
+            graphics2.dispose();
+        }
+
+        ImageIO.write(bufferedImage1, "png", new File("/tmp/" + System.currentTimeMillis() + ".png"));
+    }
+}
