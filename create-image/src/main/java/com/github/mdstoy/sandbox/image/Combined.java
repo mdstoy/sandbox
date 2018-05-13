4b3f52a333654ada8c30efa3bc1e19293386c14b
mdstoy <toyofyou.me@gmail.com>
2018-05-13 21:35:50 +0900

    実験中

--- create-image/src/main/java/com/github/mdstoy/sandbox/image/Combined.java
+++ create-image/src/main/java/com/github/mdstoy/sandbox/image/Combined.java
@@ -11,41 +11,9 @@ public class Combined {
 
     public static void main(String[] args) throws Exception {
         new Combined().execute();
-        new Combined().execute2();
     }
 
     public void execute() throws Exception{
-
-        BufferedImage bufferedImage1 = ImageIO.read(new File("back.png"));
-        BufferedImage bufferedImage2 = ImageIO.read(new File("down.png"));
-
-        Graphics2D graphics1 = null;
-        Graphics2D graphics2 = null;
-
-        try {
-
-            graphics1 = bufferedImage1.createGraphics();
-
-            graphics1.drawImage(bufferedImage2, 0, 0, null);
-
-            BufferedImage bufferedImage3 = new BufferedImage(48, 48, BufferedImage.TYPE_INT_ARGB);
-            graphics2 = bufferedImage2.createGraphics();
-            AffineTransform at = new AffineTransform();
-            at.setToRotation(Math.toRadians(90), 24d, 24d);
-            AffineTransformOp op = new AffineTransformOp(at, AffineTransformOp.TYPE_BICUBIC);
-            op.filter(bufferedImage2, bufferedImage3);
-            graphics1.drawImage(bufferedImage3, 48, 0, null);
-
-        } finally {
-
-            graphics1.dispose();
-            graphics2.dispose();
-        }
-
-        ImageIO.write(bufferedImage1, "png", new File("/tmp/" + System.currentTimeMillis() + ".png"));
-    }
-
-    public void execute2() throws Exception{
 
         BufferedImage bufferedImage1 = ImageIO.read(new File("back.png"));
         BufferedImage bufferedImage2 = ImageIO.read(new File("down.png"));
@@ -74,6 +42,8 @@ public class Combined {
             // 変換元 -> 変換先
             op.filter(bufferedImage2, bufferedImage3);
             graphics1.drawImage(bufferedImage3, 92, 48, null);
+            bufferedImage3 = changeColor(bufferedImage3);
+            graphics1.drawImage(bufferedImage3, 48, 66, null);
 
         } finally {
 
@@ -82,5 +52,20 @@ public class Combined {
         }
 
         ImageIO.write(bufferedImage1, "png", new File("/tmp/" + System.currentTimeMillis() + ".png"));
+    }
+
+    private BufferedImage changeColor(BufferedImage image) {
+        for(int i = 0; i < image.getHeight(); ++i) {
+            int rgb = image.getRGB(i, 24);
+            System.out.println(rgb + "/" + ((rgb >> 16) & 0xff) + "/" + ((rgb >> 8) & 0xff) + "/" + (rgb & 0xff));
+            for(int j = 0; j < image.getWidth(); ++j) {
+                if (image.getRGB(i, j) == -16777216) {
+                    image.setRGB(i, j, (128 << 16) + (16 << 8) + 16);
+                }
+            }
+        }
+        System.out.println((128 << 16) + (16 << 8) + 16);
+
+        return image;
     }
 }
